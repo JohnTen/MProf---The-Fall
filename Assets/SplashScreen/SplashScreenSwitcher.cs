b22c837913b0055dbf56060c5dcdc51d1f67ed4b
Preyas <preyas.yande88@gmail.com>
2018-11-06 01:05:40 +0000

    SplashScreen

---
+++ Assets/SplashScreen/SplashScreenSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenSwitcher : MonoBehaviour {
+
+    public static int SceneNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SceneNumber == 0)
+        {
+            StartCoroutine(ToMainMenu());
+        }
+
+    }
+
+    IEnumerator ToMainMenu()
+    {
+        yield return new WaitForSeconds(10);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+    }
+}
