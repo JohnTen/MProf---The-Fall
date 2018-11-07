f89ae1eb558403e7fde2088c842fb702f3d2659d
Bryanp2408 <43380465+Bryanp2408@users.noreply.github.com>
2018-11-07 16:33:39 +0000

    Fixes

--- Assets/SplashScreen/SplashScreenSwitcher.cs
+++ Assets/SplashScreen/SplashScreenSwitcher.cs
@@ -5,23 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SplashScreenSwitcher : MonoBehaviour {
-
-    public static int SceneNumber;
-
+    
     // Use this for initialization
     void Start()
     {
-        if (SceneNumber == 0)
-        {
-            StartCoroutine(ToMainMenu());
-        }
-
+        StartCoroutine(ToMainMenu());
     }
 
     IEnumerator ToMainMenu()
     {
         yield return new WaitForSeconds(10);
-        SceneNumber = 1;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("TitleScreen");
     }
 }
